f2493a188d0a3c3047533ba2c7caf5bc56b6be8c
spyros-zikos <zikossp@gmail.com>
2025-03-06 13:07:54 +0200

    sd5

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -7,8 +7,11 @@ const nextConfig = {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
   eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreDuringBuilds: true,
   },
+  // eslint: {
+  //   ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  // },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
